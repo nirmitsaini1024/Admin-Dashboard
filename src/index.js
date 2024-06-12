939e94cd0fbcd38950851018ffa0932d7c9e43ed
Nirmit Saini <122887695+nirmitsaini1024@users.noreply.github.com>
2024-06-12 14:05:19 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { ContextProvider } from './contexts/ContextProvider';
+
+import App from './App';
+
+ReactDOM.render(
+    <ContextProvider>
+        <App />
+    </ContextProvider>, document.getElementById('root'));
